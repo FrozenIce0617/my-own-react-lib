fb9583d6af4df118b50277601aebbbb7bc65cc79
SuccessTracker <prosdev087@gmail.com>
2019-03-11 18:12:22 -0400

    setup: set config of webpack and bable, enable to bundle jsx elements

---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+// Webpack uses this to work with directories
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+// This is the main configuration object.
+module.exports = {
+  entry: './src/index.js',
+
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '',
+    filename: 'bundle.js',
+  },
+
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'public'),
+    },
+    compress: true,
+    port: 9000,
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, 'public', 'index.html'),
+    }),
+  ],
+
+  // Default mode for Webpack is production.
+  // Depending on mode Webpack will apply different things
+  // on the final bundle. For now, we don't need production's JavaScript
+  // minifying and other things, so let's set mode to development
+  mode: 'development',
+}
