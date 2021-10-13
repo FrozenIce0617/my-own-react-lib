8598b5813fdbcbcb8b7fcb480944f6946143c9b2
SuccessTracker <prosdev087@gmail.com>
2021-10-13 10:23:52 -0400

    feat: add support of style props

--- src/index.js
+++ src/index.js
@@ -4,7 +4,11 @@ const root = document.getElementById('root')
 
 function App() {
   const [state, setState] = MyOwnReact.useState(1)
-  return <h1 onClick={() => setState((c) => c + 1)}>Count: {state}</h1>
+  return (
+    <h1 onClick={() => setState((c) => c + 1)} style={{ color: 'red' }}>
+      Count: {state}
+    </h1>
+  )
 }
 
 MyOwnReact.render(<App />, root)
