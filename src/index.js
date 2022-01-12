7373f689f70d437f40e1042367851593dcd379c7
SuccessTracker <prosdev087@gmail.com>
2022-01-12 12:25:32 -0500

    feat: add support of useEffect hook

--- src/index.js
+++ src/index.js
@@ -4,6 +4,11 @@ const root = document.getElementById('root')
 
 function App() {
   const [state, setState] = MyOwnReact.useState(1)
+  
+  MyOwnReact.useEffect(() => {
+    console.info(state)
+  }, [state])
+
   return (
     <h1 onClick={() => setState((c) => c + 1)} style={{ color: 'red' }}>
       Count: {state}
